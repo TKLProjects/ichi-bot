b0b75181d9c8435291a255a066be26bc7011703b
TheSparkstarScope <53199636+TheSparkstarScope@users.noreply.github.com>
2020-05-21 17:58:53 -0300

    Added about command

--- sparkcode-v1.py
+++ sparkcode-v1.py
@@ -11,6 +11,10 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
+@client.command()
+async def about(ctx):
+    await ctx.send("```SparkBot™, by the loud-mouthed guy with little knowledge in programming - A.K.A Ichiki Hayaite#1111, special thanks to Lexi#2249.```")
+
 @client.command(name="secret",hidden=True)
 async def heehoo(ctx):
     await ctx.send("Super command yo - It's a secret to everybody!")
